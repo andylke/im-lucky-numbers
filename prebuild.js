0e704097e9e6fb8a015a327f6634d4ca4462c751
Andy Lian <andylke@gmail.com>
2025-08-09 19:28:46 +0800

    Remove workspace, versionCode.

--- prebuild.js
+++
@@ -1,8 +0,0 @@
-const fs = require("fs");
-
-// Update android.versionCode
-const appJsonPath = "./app.json";
-const appJson = JSON.parse(fs.readFileSync(appJsonPath, "utf-8"));
-let [major, minor, patch] = appJson.expo.version.split(".").map(Number);
-appJson.expo.android.versionCode = (major * 10000) + (minor * 100) + patch;
-fs.writeFileSync(appJsonPath, JSON.stringify(appJson, null, 2));
